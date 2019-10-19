b19df8cba27f999fec9f4e3bffc37f1b20a6f38d
sparkofcreativity <54905996+sparkofcreativity@users.noreply.github.com>
2019-10-18 21:12:25 -0600

    Update index-test.js

--- index-test.js
+++ index-test.js
@@ -7,4 +7,5 @@ const timeline1 = new TimelineMax();
 timeline1.fromTo(hero, 1, {height: "0%"}, {height: "60%", ease: Power2.easeInOut})
 .fromTo(hero, 1.2, {width: "100%"}, {width: "70%", ease: Power2.easeInOut}, "-=1.2")
 .fromTo(hero, 1, {height: "60%"}, {height: "100%", ease: Power2.easeInOut})
-.fromTo(hero, 1.2, {width: "70%"}, {width: "100%", ease: Power2.easeInOut});
+.fromTo(hero, 1.2, {width: "70%"}, {width: "100%", ease: Power2.easeInOut})
+.fromTo(hero, 1.2, {height: "100%"}, {height: "90%", ease: Power2.easeInOut});
